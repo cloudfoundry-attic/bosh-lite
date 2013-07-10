576786b112b3743edfa423eed3e2d4cbc8b1e9b3
Gary Liu <garyliu@rbcon.com>
2013-07-10 09:21:45 +0000

    suppress wget output

--- scripts/aws_customize.sh
+++ scripts/aws_customize.sh
@@ -5,7 +5,7 @@ apt-get -y install rubygems1.9.1
 grep -q gems/1.9.1 /etc/profile || echo 'export PATH=$PATH:/var/lib/gems/1.9.1/bin' >> /etc/profile
 
 # Redis 2.x installed for lucid
-wget -c http://mirror.pnl.gov/ubuntu//pool/universe/r/redis/redis-server_2.2.12-1build1_amd64.deb -O /tmp/redis-2.2.12.deb
+wget --no-verbose -c http://mirror.pnl.gov/ubuntu//pool/universe/r/redis/redis-server_2.2.12-1build1_amd64.deb -O /tmp/redis-2.2.12.deb
 dpkg -i /tmp/redis-2.2.12.deb
 
 which chef-solo || gem install chef --no-ri --no-rdoc -v 10.26.0
--- scripts/vitrualbox_lucid_customize.sh
+++ scripts/vitrualbox_lucid_customize.sh
@@ -4,7 +4,7 @@ set -x
 apt-get install -y linux-image-generic-lts-backport-natty
 
 # Redis 2.x installed for lucid
-wget -c http://mirror.pnl.gov/ubuntu//pool/universe/r/redis/redis-server_2.2.12-1build1_amd64.deb -O /tmp/redis-2.2.12.deb
+wget --no-verbose -c http://mirror.pnl.gov/ubuntu//pool/universe/r/redis/redis-server_2.2.12-1build1_amd64.deb -O /tmp/redis-2.2.12.deb
 dpkg -i /tmp/redis-2.2.12.deb
 
 gem uninstall chef
