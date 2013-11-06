77e051aaa68cdfab2e583ab5af8be4ba6fa6b72f
Gary Liu <garyliu@rbcon.com>
2013-11-06 18:04:44 +0800

    [ci] publish packer built boxes to S3

--- ci/ci-warden-cpi-box-build.sh
+++ ci/ci-warden-cpi-box-build.sh
@@ -1,14 +1,24 @@
 set -x
-set -e 
+set -e
 
-PATH=/usr/local/packer/:/home/jenkins/.rbenv/bin:/home/jenkins/.rbenv/shims:/home/jenkins/.rbenv/bin:$PATH
+PACKER_LOG=1
+#PACKER_LOG_PATH=/tmp/packer-box
+PATH=/var/lib/jenkins/.rbenv/shims:/var/lib/jenkins/.rbenv/bin:$PATH
 
 bundle
 bundle exec librarian-chef install
 
-rm -rf boxes/*/*.box
-rm -rf "/home/jenkins/VirtualBox VMs/boshlite-virtualbox-ubuntu1204"
+killall vmware-vmx || true
+if [ -f '/var/lib/jenkins/VirtualBox VMs/boshlite-virtualbox-ubuntu1204/boshlite-virtualbox-ubuntu1204.vbox' ] ; then
+  VBoxManage modifyvm boshlite-virtualbox-ubuntu1204 --natpf1 delete packerssh
+  VBoxManage unregistervm boshlite-virtualbox-ubuntu1204 --delete
+  #rm -f /var/lib/jenkins/VirtualBox VMs/boshlite-virtualbox-ubuntu1204/boshlite-virtualbox-ubuntu1204.vbox
+fi
 
 cd boxes/
-make vmware/boshlite-ubuntu1204.box
+make clean
 make virtualbox/boshlite-ubuntu1204.box
+make vmware/boshlite-ubuntu1204.box
+
+s3cmd put -P */*.box s3://bosh-lite-build-artifacts/${BUILD_NUMBER}/
+s3cmd cp -P --recursive s3://bosh-lite-build-artifacts/${BUILD_NUMBER}/ s3://bosh-lite-build-artifacts/current/
