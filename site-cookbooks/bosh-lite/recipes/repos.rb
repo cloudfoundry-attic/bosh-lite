37dbc2364388bc57eda3b5da5fdb7e6be83b7b2c
Gary Liu <garyliu@rbcon.com>
2013-11-14 21:23:28 +0800

    don't need ruby 1.9.3-p392

--- site-cookbooks/bosh-lite/recipes/repos.rb
+++ site-cookbooks/bosh-lite/recipes/repos.rb
@@ -5,11 +5,6 @@
   package package_name
 end
 
-rbenv_ruby '1.9.3-p392'
-
-rbenv_gem "bundler" do
-  ruby_version "1.9.3-p392"
-end
 
 git "/tmp/bosh-lite" do
   repository "git://github.com/cloudfoundry/bosh-lite.git"
@@ -20,5 +15,3 @@ git "/tmp/bosh-lite/cf-release" do
   repository "git://github.com/cloudfoundry/cf-release.git"
   action :sync
 end
-
-cookbook_file "/tmp/dev.yml"
