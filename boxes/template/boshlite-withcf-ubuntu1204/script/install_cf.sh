f55388fb59867b7e23f9e27eefda8df10718fc9f
Kai Xiang <kxiang@pivotallabs.com>
2013-11-25 17:11:46 +0800

    mcf box build now can specify cf-release version

--- boxes/template/boshlite-withcf-ubuntu1204/script/install_cf.sh
+++ boxes/template/boshlite-withcf-ubuntu1204/script/install_cf.sh
@@ -16,6 +16,11 @@ bosh -u admin -p admin -n upload stemcell /tmp/latest-bosh-stemcell-warden.tgz
 (
   cd cf-release
   git checkout deployed-to-prod
+  last=` tail -1 releases/index.yml |grep -Po "\d+"`
+  ref=${CF_VERSION:-"$last"}
+  git checkout v${ref}
+  cmd="bosh -u admin -p admin -n upload release releases/cf-${ref}.yml"
+  $cmd || bosh releases | grep cf || $cmd
 )
 
 wget  -r --tries=10 https://github.com/vito/spiff/releases/download/v0.2/spiff_linux_amd64 -O /usr/local/bin/spiff
@@ -25,9 +30,8 @@ CF_RELEASE_DIR=cf-release ./scripts/make_manifest_spiff
 
 (
   cd cf-release
-  last=` tail -1 releases/index.yml |grep -Po "\d+"`
-  bosh -u admin -p admin -n upload release releases/cf-${last}.yml
-  bosh -u admin -p admin -n deploy
+  cmd="bosh -u admin -p admin -n deploy"
+  $cmd || $cmd
 )
 
 sleep 30
