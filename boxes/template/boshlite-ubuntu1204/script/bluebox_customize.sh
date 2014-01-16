bbb78b01dec56220d763e7c85727ad28cf010c19
Kai Xiang <kxiang@pivotallabs.com>
2014-01-16 05:22:32 +0000

    openstack packer template for CI

---
+++ boxes/template/boshlite-ubuntu1204/script/bluebox_customize.sh
@@ -0,0 +1,9 @@
+# Set up sudo - base careful to set the file attribue before copying to
+# sudoers.d
+
+apt-get -y update
+#apt-get -y upgrade
+#apt-get -y install curl
+apt-get -y install make
+apt-get -y install libpq-dev
+apt-get clean
