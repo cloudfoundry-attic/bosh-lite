d24d865d06065132584c644975d84ca9ceef73d9
Gary Liu <garyliu@pivotallabs.com>
2014-02-21 15:36:23 +0800

    fix bosh_cli version

--- site-cookbooks/bosh-lite/recipes/bosh_cli.rb
+++ site-cookbooks/bosh-lite/recipes/bosh_cli.rb
@@ -2,10 +2,8 @@ include_recipe 'bosh-lite::rbenv'
 
 rbenv_gem 'bosh_cli' do
   ruby_version "1.9.3-p448"
-  version '~>1.5.0.pre.847'
 end
 
 rbenv_gem 'bosh_cli' do
   ruby_version "1.9.3-p484"
-  version '~>1.5.0.pre.847'
 end
