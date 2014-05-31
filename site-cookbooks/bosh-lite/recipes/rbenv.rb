aecf8d53781a7800520bc6cb312f7c9d1079ef1e
Adam Stegman <astegman@pivotallabs.com>
2014-05-30 17:54:00 -0700

    Install the ruby version that warden uses.

--- site-cookbooks/bosh-lite/recipes/rbenv.rb
+++ site-cookbooks/bosh-lite/recipes/rbenv.rb
@@ -1,17 +1,11 @@
 include_recipe 'rbenv::default'
 include_recipe 'rbenv::ruby_build'
 
-rbenv_ruby '1.9.3-p484' do
+rbenv_ruby '1.9.3-p574' do
   global true
 end
 
-rbenv_ruby '1.9.3-p448'
-
 rbenv_gem "bundler" do
-  ruby_version "1.9.3-p448"
-end
-
-rbenv_gem "bundler" do
-  ruby_version "1.9.3-p484"
+  ruby_version "1.9.3-p574"
 end
 
