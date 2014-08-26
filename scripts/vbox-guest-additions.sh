88f44381e886f14c0d80bcad69378066d56890fc
Andrea Law <alaw@pivotallabs.com>
2014-08-26 10:47:11 -0700

    Remove commented lines in vbox-guest-additions
    Signed-off-by: Kris Hicks <krishicks@gmail.com>

--- scripts/vbox-guest-additions.sh
+++ scripts/vbox-guest-additions.sh
@@ -5,10 +5,7 @@ set -ex
 apt-get install -y linux-headers-$(uname -r) build-essential make perl dkms puppet-common
 apt-get -y clean
 
-#VBOX_VERSION=$(cat /home/vagrant/.vbox_version)
 mount -o loop /home/vagrant/VBoxGuestAdditions.iso /media/cdrom
 # Ignore X driver failure, we don't need it
 /media/cdrom/VBoxLinuxAdditions.run || true
 umount /media/cdrom
-
-#rm /tmp/VBoxGuestAdditions.iso
