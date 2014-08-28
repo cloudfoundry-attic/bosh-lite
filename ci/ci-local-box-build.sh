0709854cabd59fe6a0639ef1478cf149a305c44f
Andrea Law <alaw@pivotallabs.com>
2014-08-28 11:55:29 -0700

    ci-local-box-build rms old .vbox files again
    Signed-off-by: Kris Hicks <krishicks@gmail.com>

--- ci/ci-local-box-build.sh
+++ ci/ci-local-box-build.sh
@@ -6,6 +6,12 @@ git submodule update --init --recursive
 
 PACKER_LOG=1
 
+if [ -f '/var/lib/jenkins/VirtualBox VMs/packer-virtualbox-iso/packer-virtualbox-iso.vbox' ] ; then
+  VBoxManage modifyvm packer-virtualbox-iso --natpf1 delete packerssh || true
+  VBoxManage unregistervm packer-virtualbox-iso --delete || true
+  rm -f '/var/lib/jenkins/VirtualBox VMs/packer-virtualbox-iso/packer-virtualbox-iso.vbox'
+fi
+
 ./bin/build-${BOX_TYPE} ${BOSH_RELEASE_VERSION} ${BOSH_RELEASE_BUILD_NUMBER} ${WARDEN_RELEASE_VERSION} ${BUILD_NUMBER}
 
 vagrant box add bosh-lite-${BOX_TYPE}-ubuntu-trusty-${BUILD_NUMBER}.box --name bosh-lite-virtualbox-ubuntu-trusty --force
