24b3696f35b8c3224c7b3e2f85c637f1872b93d7
Kris Hicks <krishicks@gmail.com>
2014-09-10 15:48:31 -0700

    Update ci-aws-box-build
    * Clean up old box files
    * Don't update bosh-lite-ami.list
    
    Signed-off-by: Adam Stegman <astegman@pivotallabs.com>

--- ci/ci-aws-box-build.sh
+++ ci/ci-aws-box-build.sh
@@ -1,8 +1,10 @@
-set -x
-set -e
+#!/usr/bin/env bash
+set -ex
+
+# Clean up old box files
+rm -rf *.box
 
 PACKER_LOG=1
-. ~/.awskey
 
 ./bin/build-aws ${BOSH_RELEASE_VERSION} ${BOSH_RELEASE_BUILD_NUMBER} ${WARDEN_RELEASE_VERSION} ${BUILD_NUMBER} | tee output
 
@@ -10,8 +12,3 @@ ami=`tail -2 output | grep -Po "ami-.*"`
 
 sleep 60
 ec2-modify-image-attribute $ami  --launch-permission -a all --aws-access-key $AWS_ACCESS_KEY_ID --aws-secret-key $AWS_SECRET_ACCESS_KEY
-
-s3cmd get --force s3://bosh-lite-build-artifacts/ami/bosh-lite-ami.list bosh-lite-ami.list
-echo $ami >> bosh-lite-ami.list
-s3cmd put -P  bosh-lite-ami.list s3://bosh-lite-build-artifacts/ami/bosh-lite-ami.list
-
