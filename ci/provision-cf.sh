2aa7aeb9ef55776a7980a0958f2dbd01285c1eac
Andrei Dinin <adinin@pivotal.io>
2014-09-09 18:01:32 -0700

    Clone cf-release for provision_cf tests
    Signed-off-by: Adam Stegman <astegman@pivotal.io>

--- ci/provision-cf.sh
+++ ci/provision-cf.sh
@@ -9,6 +9,10 @@ cleanup() {
 
 trap cleanup EXIT
 
+if [ ! -d '../cf-release' ]; then
+  git clone --depth=1 https://github.com/cloudfoundry/cf-release.git ../cf-release
+fi
+
 vagrant up --provider=virtualbox
 
 set +e
