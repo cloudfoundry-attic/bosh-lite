2cfb23143223ef495140ea84f5863032922dc96c
Kris Hicks <krishicks@gmail.com>
2014-09-11 09:47:37 -0700

    Use BOSH_LITE_CANDIDATE_BUILD_NUMBER in build scripts
    CANDIDATE_BUILD_NUMBER is reserved by bosh.
    
    Signed-off-by: Adam Stegman <astegman@pivotallabs.com>

--- ci/ci-aws-box-bats.sh
+++ ci/ci-aws-box-bats.sh
@@ -7,11 +7,11 @@ trap cleanup EXIT
 
 clean_vagrant
 
-sed -i'' -e "s/\.box = .*/.box = 'bosh-lite-ubuntu-trusty-aws-$CANDIDATE_BUILD_NUMBER'/" Vagrantfile
+sed -i'' -e "s/\.box = .*/.box = 'bosh-lite-ubuntu-trusty-aws-$BOSH_LITE_CANDIDATE_BUILD_NUMBER'/" Vagrantfile
 cat Vagrantfile
 
-box_add_and_vagrant_up aws aws $CANDIDATE_BUILD_NUMBER
+box_add_and_vagrant_up aws aws $BOSH_LITE_CANDIDATE_BUILD_NUMBER
 
 run_bats_on_vm
 
-publish_vagrant_box aws $CANDIDATE_BUILD_NUMBER
+publish_vagrant_box aws $BOSH_LITE_CANDIDATE_BUILD_NUMBER
