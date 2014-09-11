c1d20cae39e7fdaf2e248a03ebd67729053a4903
Kris Hicks <krishicks@gmail.com>
2014-09-11 09:47:37 -0700

    Use BOSH_LITE_CANDIDATE_BUILD_NUMBER in build scripts
    CANDIDATE_BUILD_NUMBER is reserved by bosh.
    
    Signed-off-by: Adam Stegman <astegman@pivotallabs.com>

--- ci/ci-box-bats.sh
+++ ci/ci-box-bats.sh
@@ -7,16 +7,16 @@ trap cleanup EXIT
 
 clean_vagrant
 
-sed -i'' -e "s/\.box = .*/.box = 'bosh-lite-ubuntu-trusty-$BOX_TYPE-$CANDIDATE_BUILD_NUMBER'/" Vagrantfile
+sed -i'' -e "s/\.box = .*/.box = 'bosh-lite-ubuntu-trusty-$BOX_TYPE-$BOSH_LITE_CANDIDATE_BUILD_NUMBER'/" Vagrantfile
 PRIVATE_NETWORK_IP=${PRIVATE_NETWORK_IP:-192.168.50.4}
 sed -i'' -e "s/# override\.vm\.network :private_network, ip: '192\.168\.54\.4', id: :local/override.vm.network :private_network, ip: '$PRIVATE_NETWORK_IP', id: :local/" Vagrantfile
 cat Vagrantfile
 sed -i'' -e "s/192.168.50.4/$PRIVATE_NETWORK_IP/" bin/add-route
 cat bin/add-route
 
-box_add_and_vagrant_up $BOX_TYPE $PROVIDER $CANDIDATE_BUILD_NUMBER
+box_add_and_vagrant_up $BOX_TYPE $PROVIDER $BOSH_LITE_CANDIDATE_BUILD_NUMBER
 
 ./bin/add-route || true
 run_bats_against $PRIVATE_NETWORK_IP
 
-publish_vagrant_box $BOX_TYPE $CANDIDATE_BUILD_NUMBER
+publish_vagrant_box $BOX_TYPE $BOSH_LITE_CANDIDATE_BUILD_NUMBER
--- ci/publish-stemcells.sh
+++ ci/publish-stemcells.sh
@@ -3,9 +3,9 @@ set -ex
 
 env | sort
 
-s3cmd put -P ./bosh-stemcell-$CANDIDATE_BUILD_NUMBER-warden-boshlite-ubuntu-trusty-go_agent.tgz s3://bosh-jenkins-artifacts/bosh-stemcell/warden/
-s3cmd put -P ./bosh-stemcell-$CANDIDATE_BUILD_NUMBER-warden-boshlite-ubuntu-trusty-go_agent.tgz s3://bosh-jenkins-artifacts/bosh-stemcell/warden/latest-bosh-stemcell-warden-ubuntu-trusty-go_agent.tgz
+s3cmd put -P ./bosh-stemcell-$BOSH_LITE_CANDIDATE_BUILD_NUMBER-warden-boshlite-ubuntu-trusty-go_agent.tgz s3://bosh-jenkins-artifacts/bosh-stemcell/warden/
+s3cmd put -P ./bosh-stemcell-$BOSH_LITE_CANDIDATE_BUILD_NUMBER-warden-boshlite-ubuntu-trusty-go_agent.tgz s3://bosh-jenkins-artifacts/bosh-stemcell/warden/latest-bosh-stemcell-warden-ubuntu-trusty-go_agent.tgz
 
 # trusty is the default ubuntu stemcell
-s3cmd put -P ./bosh-stemcell-$CANDIDATE_BUILD_NUMBER-warden-boshlite-ubuntu-trusty-go_agent.tgz s3://bosh-jenkins-artifacts/bosh-stemcell/warden/latest-bosh-stemcell-warden-ubuntu.tgz
-s3cmd put -P ./bosh-stemcell-$CANDIDATE_BUILD_NUMBER-warden-boshlite-ubuntu-trusty-go_agent.tgz s3://bosh-jenkins-artifacts/bosh-stemcell/warden/latest-bosh-stemcell-warden.tgz
+s3cmd put -P ./bosh-stemcell-$BOSH_LITE_CANDIDATE_BUILD_NUMBER-warden-boshlite-ubuntu-trusty-go_agent.tgz s3://bosh-jenkins-artifacts/bosh-stemcell/warden/latest-bosh-stemcell-warden-ubuntu.tgz
+s3cmd put -P ./bosh-stemcell-$BOSH_LITE_CANDIDATE_BUILD_NUMBER-warden-boshlite-ubuntu-trusty-go_agent.tgz s3://bosh-jenkins-artifacts/bosh-stemcell/warden/latest-bosh-stemcell-warden.tgz
