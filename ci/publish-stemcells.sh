5832917d400d21dbe808fe568f77f90b88fd0f60
Kris Hicks <krishicks@gmail.com>
2014-09-16 16:34:09 -0700

    Add CentOS stemcells to publish-stemcells.sh
    Signed-off-by: Andrea Law <alaw@pivotallabs.com>

--- ci/publish-stemcells.sh
+++ ci/publish-stemcells.sh
@@ -3,8 +3,13 @@ set -ex
 
 env | sort
 
+# Trusty
 s3cmd put -P ./bosh-stemcell-$BOSH_LITE_CANDIDATE_BUILD_NUMBER-warden-boshlite-ubuntu-trusty-go_agent.tgz s3://bosh-jenkins-artifacts/bosh-stemcell/warden/
 s3cmd put -P ./bosh-stemcell-$BOSH_LITE_CANDIDATE_BUILD_NUMBER-warden-boshlite-ubuntu-trusty-go_agent.tgz s3://bosh-jenkins-artifacts/bosh-stemcell/warden/latest-bosh-stemcell-warden-ubuntu-trusty-go_agent.tgz
+
+# CentOS
+s3cmd put -P ./bosh-stemcell-$BOSH_LITE_CANDIDATE_BUILD_NUMBER-warden-boshlite-centos-go_agent.tgz s3://bosh-jenkins-artifacts/bosh-stemcell/warden/
+s3cmd put -P ./bosh-stemcell-$BOSH_LITE_CANDIDATE_BUILD_NUMBER-warden-boshlite-centos-go_agent.tgz s3://bosh-jenkins-artifacts/bosh-stemcell/warden/latest-bosh-stemcell-warden-centos-go_agent.tgz
 
 # trusty is the default ubuntu stemcell
 s3cmd put -P ./bosh-stemcell-$BOSH_LITE_CANDIDATE_BUILD_NUMBER-warden-boshlite-ubuntu-trusty-go_agent.tgz s3://bosh-jenkins-artifacts/bosh-stemcell/warden/latest-bosh-stemcell-warden-ubuntu.tgz
