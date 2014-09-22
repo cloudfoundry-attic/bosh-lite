08498c67c0680072096b811400087e15ef46e625
Maria Shaldibina <mariash@pivotal.io>
2014-09-22 13:37:23 -0700

    Add bin/add-route to ci-stemcell-bats
    Signed-off-by: Kris Hicks <krishicks@gmail.com>

--- ci/ci-stemcell-bats.sh
+++ ci/ci-stemcell-bats.sh
@@ -11,6 +11,8 @@ vagrant up --provider=virtualbox
 
 rm -rf output
 
+./bin/add-route || true
+
 export BAT_STEMCELL=$(pwd)/bosh-stemcell-$BOSH_LITE_CANDIDATE_BUILD_NUMBER-warden-boshlite-$STEMCELL_OS_NAME-go_agent.tgz
 run_bats_against 192.168.50.4 $STEMCELL_OS_NAME
 
