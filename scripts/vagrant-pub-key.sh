a3babbb72b5d7250fc23f3337e9da32048313f73
Kris Hicks <krishicks@gmail.com>
2014-09-25 09:13:50 -0700

    Set permissions on ~/.ssh and authorized_keys
    Signed-off-by: Maria Shaldibina <mshaldibina@pivotal.io>

--- scripts/vagrant-pub-key.sh
+++ scripts/vagrant-pub-key.sh
@@ -2,6 +2,8 @@
 
 set -ex
 
-mkdir -p ~/.ssh
+mkdir -m 700 -p ~/.ssh
 
 wget -qO- https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub >> ~/.ssh/authorized_keys
+
+chmod 600 ~/.ssh/authorized_keys
