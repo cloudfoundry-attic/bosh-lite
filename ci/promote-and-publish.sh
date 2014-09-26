aa650b231e6102eeadda4c3a83b0864ff6df8559
Maria Shaldibina <mshaldibina@pivotal.io>
2014-09-26 11:01:18 -0700

    Update ci/promote-and-publish
    Update Vagrant Cloud box version
    
    Signed-off-by: Kris Hicks <krishicks@gmail.com>

--- ci/promote-and-publish.sh
+++ ci/promote-and-publish.sh
@@ -14,10 +14,10 @@ publish_vagrant_box aws $BOSH_LITE_CANDIDATE_BUILD_NUMBER
 publish_vagrant_box vmware $BOSH_LITE_CANDIDATE_BUILD_NUMBER
 publish_vagrant_box virtualbox $BOSH_LITE_CANDIDATE_BUILD_NUMBER
 
-sed -i'' -e "s/[0-9]+\.box/${BOSH_LITE_CANDIDATE_BUILD_NUMBER}.box/" Vagrantfile
+sed -i'' -e "s/config.vm.box_version = '.*'/config.vm.box_version = '$BOSH_LITE_CANDIDATE_BUILD_NUMBER'/" Vagrantfile
 git diff
 git add Vagrantfile
-git commit -m "Update box versions to $BOSH_LITE_CANDIDATE_BUILD_NUMBER"
+git commit -m "Update box version to $BOSH_LITE_CANDIDATE_BUILD_NUMBER"
 git push origin HEAD:master
 
 git fetch origin develop
