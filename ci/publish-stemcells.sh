996b73f1639277f22beba9b28c5e7e4d516198c1
Andrea Law <alaw@pivotal.io>
2014-09-30 16:31:36 -0700

    Refactor to reduce duplicated lines
    Signed-off-by: Andrei Dinin <adinin@pivotal.io>

--- ci/publish-stemcells.sh
+++ ci/publish-stemcells.sh
@@ -3,13 +3,10 @@ set -ex
 
 env | sort
 
-# Trusty
-s3cmd put --mime-type=application/x-gtar --no-preserve -P ./bosh-stemcell-$BOSH_LITE_CANDIDATE_BUILD_NUMBER-warden-boshlite-ubuntu-trusty-go_agent.tgz s3://bosh-jenkins-artifacts/bosh-stemcell/warden/
-s3cmd put --mime-type=application/x-gtar --no-preserve -P ./bosh-stemcell-$BOSH_LITE_CANDIDATE_BUILD_NUMBER-warden-boshlite-ubuntu-trusty-go_agent.tgz s3://bosh-jenkins-artifacts/bosh-stemcell/warden/latest-bosh-stemcell-warden-ubuntu-trusty-go_agent.tgz
-
-# CentOS
-s3cmd put --mime-type=application/x-gtar --no-preserve -P ./bosh-stemcell-$BOSH_LITE_CANDIDATE_BUILD_NUMBER-warden-boshlite-centos-go_agent.tgz s3://bosh-jenkins-artifacts/bosh-stemcell/warden/
-s3cmd put --mime-type=application/x-gtar --no-preserve -P ./bosh-stemcell-$BOSH_LITE_CANDIDATE_BUILD_NUMBER-warden-boshlite-centos-go_agent.tgz s3://bosh-jenkins-artifacts/bosh-stemcell/warden/latest-bosh-stemcell-warden-centos-go_agent.tgz
+for os in "ubuntu-trusty" "centos"; do
+  s3cmd put --mime-type=application/x-gtar --no-preserve -P ./bosh-stemcell-$BOSH_LITE_CANDIDATE_BUILD_NUMBER-warden-boshlite-$os-go_agent.tgz s3://bosh-jenkins-artifacts/bosh-stemcell/warden/
+  s3cmd put --mime-type=application/x-gtar --no-preserve -P ./bosh-stemcell-$BOSH_LITE_CANDIDATE_BUILD_NUMBER-warden-boshlite-$os-go_agent.tgz s3://bosh-jenkins-artifacts/bosh-stemcell/warden/latest-bosh-stemcell-warden-$os-go_agent.tgz
+done
 
 # trusty is the default ubuntu stemcell
 s3cmd put -P ./bosh-stemcell-$BOSH_LITE_CANDIDATE_BUILD_NUMBER-warden-boshlite-ubuntu-trusty-go_agent.tgz s3://bosh-jenkins-artifacts/bosh-stemcell/warden/latest-bosh-stemcell-warden-ubuntu.tgz
