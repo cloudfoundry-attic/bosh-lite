31b3eff8d5be021bf2d5e25efeaadd86487408ef
Andrea Law <alaw@pivotal.io>
2014-11-06 11:17:00 -0500

    Download stemcell from s3 to run BATs
    Signed-off-by: Andrei Dinin <adinin@pivotal.io>

--- ci/ci-stemcell-bats.sh
+++ ci/ci-stemcell-bats.sh
@@ -15,7 +15,11 @@ rm -rf output
 
 ./bin/add-route || true
 
-export BAT_STEMCELL=$(pwd)/bosh-stemcell-$BOSH_LITE_CANDIDATE_BUILD_NUMBER-warden-boshlite-$STEMCELL_OS_NAME-go_agent.tgz
+STEMCELL="bosh-stemcell-${BOSH_LITE_CANDIDATE_BUILD_NUMBER}-warden-boshlite-${STEMCELL_OS_NAME}-go_agent.tgz"
+rm -f bosh-stemcell*
+wget "https://bosh-lite-ci-pipeline.s3.amazonaws.com/${BOSH_LITE_CANDIDATE_BUILD_NUMBER}/bosh-stemcell/warden/${STEMCELL}"
+
+export BAT_STEMCELL=$(pwd)/${STEMCELL}
 run_bats_against 192.168.50.4 $STEMCELL_OS_NAME
 
 mkdir -p output
