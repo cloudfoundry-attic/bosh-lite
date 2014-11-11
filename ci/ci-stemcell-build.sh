8a03af0c3443f7da0569b1042a3020a19381019e
Andrea Law <alaw@pivotal.io>
2014-11-11 11:54:15 -0500

    Remove extra code
    Signed-off-by: Andrei Dinin <adinin@pivotal.io>

--- ci/ci-stemcell-build.sh
+++ ci/ci-stemcell-build.sh
@@ -21,6 +21,3 @@ fetch_latest_bosh
   cd bosh
   bundle exec rake ci:publish_stemcell_in_vm[warden,boshlite,$OS_NAME,$OS_VERSION,remote,go,bosh-os-images,bosh-$OS_NAME-$OS_VERSION-os-image.tgz,bosh-lite-ci-pipeline]
 )
-
-mkdir -p output
-cp /mnt/stemcells/warden/boshlite/$OS_NAME/work/work/*.tgz output/
