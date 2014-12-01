79030ba281173eac6b2a95315ba8e033f66f1594
Andrea Law <alaw@pivotal.io>
2014-12-01 10:59:48 -0500

    Update publish-stemcells for GoCD
    * Move from pipeline bucket to final bucket instead of uploading
    
    Signed-off-by: Andrei Dinin <adinin@pivotal.io>

--- ci/publish-stemcells.sh
+++ ci/publish-stemcells.sh
@@ -3,11 +3,24 @@ set -ex
 
 env | sort
 
-for os in "ubuntu-trusty" "centos"; do
-  s3cmd put --mime-type=application/x-gtar --no-preserve -P ./bosh-stemcell-$BOSH_LITE_CANDIDATE_BUILD_NUMBER-warden-boshlite-$os-go_agent.tgz s3://bosh-jenkins-artifacts/bosh-stemcell/warden/
-  s3cmd put --mime-type=application/x-gtar --no-preserve -P ./bosh-stemcell-$BOSH_LITE_CANDIDATE_BUILD_NUMBER-warden-boshlite-$os-go_agent.tgz s3://bosh-jenkins-artifacts/bosh-stemcell/warden/latest-bosh-stemcell-warden-$os-go_agent.tgz
-done
+s3_cmd_cp() {
+  s3cmd --access_key=$BOSH_AWS_ACCESS_KEY_ID --secret_key=$BOSH_AWS_SECRET_ACCESS_KEY --mime-type=application/x-gtar --no-preserve -P cp $1 $2
+}
 
-# trusty is the default ubuntu stemcell
-s3cmd put -P ./bosh-stemcell-$BOSH_LITE_CANDIDATE_BUILD_NUMBER-warden-boshlite-ubuntu-trusty-go_agent.tgz s3://bosh-jenkins-artifacts/bosh-stemcell/warden/latest-bosh-stemcell-warden-ubuntu.tgz
-s3cmd put -P ./bosh-stemcell-$BOSH_LITE_CANDIDATE_BUILD_NUMBER-warden-boshlite-ubuntu-trusty-go_agent.tgz s3://bosh-jenkins-artifacts/bosh-stemcell/warden/latest-bosh-stemcell-warden.tgz
+main() {
+  PIPELINE_BUCKET="s3://bosh-lite-ci-pipeline/$BOSH_LITE_CANDIDATE_BUILD_NUMBER/bosh-stemcell/warden"
+  STEMCELL_BUCKET="s3://bosh-warden-stemcells"
+
+  for os in "ubuntu-trusty" "centos"; do
+    STEMCELL="bosh-stemcell-${BOSH_LITE_CANDIDATE_BUILD_NUMBER}-warden-boshlite-${os}-go_agent.tgz"
+    s3_cmd_cp "${PIPELINE_BUCKET}/${STEMCELL}" "${STEMCELL_BUCKET}/"
+    s3_cmd_cp "${PIPELINE_BUCKET}/${STEMCELL}" "${STEMCELL_BUCKET}/latest-bosh-stemcell-warden-${os}-go_agent.tgz"
+  done
+
+  # trusty is the default ubuntu stemcell
+  STEMCELL="bosh-stemcell-${BOSH_LITE_CANDIDATE_BUILD_NUMBER}-warden-boshlite-ubuntu-trusty-go_agent.tgz"
+  s3_cmd_cp "${PIPELINE_BUCKET}/${STEMCELL}" "${STEMCELL_BUCKET}/latest-bosh-stemcell-warden-ubuntu.tgz"
+  s3_cmd_cp "${PIPELINE_BUCKET}/${STEMCELL}" "${STEMCELL_BUCKET}/latest-bosh-stemcell-warden.tgz"
+}
+
+main
