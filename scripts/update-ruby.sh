0582bba37feac5ad35cf73102cb94dcca6a8592b
Matthew Sykes <matthew.sykes@gmail.com>
2015-10-06 15:31:58 -0400

    Really use ruby2.0

--- scripts/update-ruby.sh
+++ scripts/update-ruby.sh
@@ -2,6 +2,12 @@
 
 set -ex
 
-apt-get -y install ruby2.0
+apt-get -y install ruby2.0 ruby2.0-dev
 
-ln -sf /usr/bin/ruby2.0 /usr/bin/ruby
+# Rename original so updates don't undo the change
+dpkg-divert --add --rename --divert /usr/bin/ruby.divert /usr/bin/ruby
+dpkg-divert --add --rename --divert /usr/bin/gem.divert /usr/bin/gem
+
+# Create an alternatives entry pointing ruby -> ruby2.0
+update-alternatives --install /usr/bin/ruby ruby /usr/bin/ruby2.0 1
+update-alternatives --install /usr/bin/gem gem /usr/bin/gem2.0 1
