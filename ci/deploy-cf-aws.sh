7465888c3857c105547a7241a899597a6d2e31a0
dmitriy kalinin <dkalinin@pivotal.io>
2015-10-08 17:50:05 -0700

    fog-google temporary hack
    Signed-off-by: Marco Voelz <marco.voelz@sap.com>

--- ci/deploy-cf-aws.sh
+++ ci/deploy-cf-aws.sh
@@ -21,6 +21,7 @@ box_add_and_vagrant_up $box_file aws aws $box_version
 
 # todo remove installation
 gem install net-ssh -v 2.10.0.beta2
+gem install fog-google -v 0.1.0
 gem install bosh_cli --no-ri --no-rdoc
 
 # Install spiff
