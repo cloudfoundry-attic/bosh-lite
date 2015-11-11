c3cc53c7e1dbdc4b3a2b6f819d0b5d822611b8af
dmitriy kalinin <dkalinin@pivotal.io>
2015-11-10 18:47:58 -0800

    use 3130 stemcell

--- ci/lib/bats.sh
+++ ci/lib/bats.sh
@@ -27,7 +27,7 @@ run_bats() {
   bundle exec bosh -c $config_file -n target $director_ip
 
   # todo temporarily point to exact stemcell until we can certify them
-  stemcell_name=bosh-stemcell-3126-warden-boshlite-ubuntu-trusty-go_agent.tgz
+  stemcell_name=bosh-stemcell-3130-warden-boshlite-ubuntu-trusty-go_agent.tgz
   wget -nv -N https://s3.amazonaws.com/bosh-warden-stemcells2/$stemcell_name
   export BAT_STEMCELL=`pwd`/$stemcell_name
 
