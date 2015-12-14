020758ca39159c2cbde7ae60b9356de70f6c0650
dmitriy kalinin <dkalinin@pivotal.io>
2015-12-14 15:56:09 -0800

    3.19 kernel hopefully

--- packer/scripts/update-trusty-kernel.sh
+++ packer/scripts/update-trusty-kernel.sh
@@ -5,7 +5,7 @@ set -e
 export UCF_FORCE_CONFFNEW=YES
 export DEBIAN_FRONTEND=noninteractive
 
-apt-get -y --force-yes install linux-generic
+apt-get -y --force-yes install linux-generic-lts-vivid
 
 reboot
 sleep 60
