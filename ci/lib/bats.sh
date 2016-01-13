6198ba09d91456938ec4639f1e11ed2942ddf253
dmitriy kalinin <dmitriy@pivotallabs.com>
2016-01-12 17:29:44 -0800

    use aws mirror for debs in tests

--- ci/lib/bats.sh
+++ ci/lib/bats.sh
@@ -65,11 +65,23 @@ EOF
 }
 
 install_bats_prereqs() {
+  sudo tee -a /etc/apt/sources.list <<EOF
+deb http://us-east-1.ec2.archive.ubuntu.com/ubuntu/ trusty main
+deb-src http://us-east-1.ec2.archive.ubuntu.com/ubuntu/ trusty main
+deb http://us-east-1.ec2.archive.ubuntu.com/ubuntu/ trusty-updates main
+deb-src http://us-east-1.ec2.archive.ubuntu.com/ubuntu/ trusty-updates main
+deb http://us-east-1.ec2.archive.ubuntu.com/ubuntu/ trusty universe
+deb-src http://us-east-1.ec2.archive.ubuntu.com/ubuntu/ trusty universe
+deb http://us-east-1.ec2.archive.ubuntu.com/ubuntu/ trusty-updates universe
+deb-src http://us-east-1.ec2.archive.ubuntu.com/ubuntu/ trusty-updates universe
+deb http://security.ubuntu.com/ubuntu trusty-security main
+deb-src http://security.ubuntu.com/ubuntu trusty-security main
+deb http://security.ubuntu.com/ubuntu trusty-security universe
+deb-src http://security.ubuntu.com/ubuntu trusty-security universe
+EOF
+
   sudo apt-get -y update
-  for i in {1..50}; do
-    sudo apt-get install -y git && break || sleep 1;
-  done
-  sudo apt-get install -y libmysqlclient-dev libpq-dev libsqlite3-dev
+  sudo apt-get install -y git libmysqlclient-dev libpq-dev libsqlite3-dev
   sudo gem install bundler --no-ri --no-rdoc
 }
 
