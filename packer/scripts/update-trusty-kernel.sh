66416cf7fefad24271f76ffbbd79b82e760412fc
dmitriy kalinin <dkalinin@pivotal.io>
2016-12-13 16:47:47 -0800

    bump to xenial

--- packer/scripts/update-trusty-kernel.sh
+++ packer/scripts/update-trusty-kernel.sh
@@ -5,7 +5,7 @@ set -ex
 export UCF_FORCE_CONFFNEW=YES
 export DEBIAN_FRONTEND=noninteractive
 
-apt-get -y --force-yes install linux-generic-lts-vivid
+apt-get -y --force-yes install linux-generic-lts-xenial
 
 reboot
 sleep 60
